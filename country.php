1b2979c9ad73ca39d9bb4a3df210ab8c88922195
SkyLight19 <khaledhesham22@gmail.com>
2015-06-26 08:24:56 +0200

    Added constructors to country class

--- country.php
+++ country.php
@@ -1,12 +1,43 @@
 <?php
 
 class Country {
-    public $code;
-    public $name;
-    public $value;
-    public $full_name;
-    public $short_name;
-    public $flag;
+    private $code;
+    private $name;
+    private $value;
+    private $curr_full;
+    private $curr_short;
+    private $flag;
+
+    public function __construct() {
+    $this->value=1;
+    }
+
+    public function __construct($country_name) {
+    	$sql = mysql_query("SELECT * FROM Countries WHERE country_name LIKE '%".$country_name."%'") or die (mysql_error());
+    	$query = mysql_query($sql);
+    	$this->name=$country_name;
+    	if ($query){
+    		$row = mysql_fetch_array($query);
+    		$this->code=$row['code'];
+    		$this->value=$row['value'];
+    		$this->curr_full=$row['curr_full'];
+    		$this->curr_short=$row['curr_short'];
+    		$this->flag=$row['flag'];
+    	}
+    	else {
+    		$this->value=1;
+    	}
+    }
+
+    public function setCountry (array $data = array()) {
+    	$this->name=$data['name'];
+    	$this->code=$data['code'];
+    	$this->value=$data['value'];
+    	$this->curr_full=$data['curr_full'];
+    	$this->curr_short=$data['curr_short'];
+    	$this->flag=$data['flag'];
+    }
+
 }
 
 ?>
