0455fe4c05c83e15745ce148ffa40e0879424199
Adham Gad <adhamgad1@gmail.com>
2015-06-26 04:59:14 +0200

    added Create country method

--- Country_Adapter.php
+++ Country_Adapter.php
@@ -1,8 +1,32 @@
 <?php
-
+/*
+country code
+county name
+currency value
+currency full name
+currency short name
+flag
+*/
 class Country_Adapter
 {
+  private $name = $_POST["name"];
+  private $code = $_POST["code"];
+  private $curr_value = $_POST["curr_value"];
+  private $curr_full = $_POST["curr_full"];
+  private $curr_short = $_POST["curr_short"];
+  private $flag = $_POST["flag"];
 
+public function Create()
+{
+  $country = new Country();
+  $country->name = $this->name;
+  $country->code = $this->code;
+  $country->curr_value = $this->curr_value;
+  $country->curr_full = $this->curr_full;
+  $country->curr_short = $this->curr_short;
+  $country->flag = $this->flag;
+  update($country);
+}
 
 
 }
