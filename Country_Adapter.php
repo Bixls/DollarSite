d2eab05704cc68a54a2dc498fe6c915aa2abc2ff
Adham Gad <adhamgad1@gmail.com>
2015-06-27 00:18:00 +0200

    added setCountry(array) create country method

--- Country_Adapter.php
+++ Country_Adapter.php
@@ -14,12 +14,16 @@ class Country_Adapter
 public function Create()
 {
   $country = new Country();
-  $country->name =  $_POST["name"];
-  $country->code = $_POST["code"];
-  $country->curr_value = $_POST["curr_value"];
-  $country->curr_full = $_POST["curr_full"];
-  $country->curr_short = $_POST["curr_short"];
-  $country->flag = $_POST["flag"];
+
+  array $country_array = array();
+  $country_array["name"] =  $_POST["name"];
+  $country_array["code"] = $_POST["code"];
+  $country_array["curr_value"] = $_POST["curr_value"];
+  $country_array["curr_full"] = $_POST["curr_full"];
+  $country_array["curr_short"] = $_POST["curr_short"];
+  $country_array["flag"] = $_POST["flag"];
+
+  $country->setCountry($country_array);
   update($country);
 }
 
