a34ef6b90c1915414b973741f38c1c4f3b3c436d
SkyLight19 <khaledhesham22@gmail.com>
2015-06-29 07:26:24 +0200

    made code primary key and used databaseconnect class

--- database.php
+++ database.php
@@ -1,14 +1,7 @@
 <?php
+require_once ("DatabaseConnect.php");
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-$connection = new mysqli($servername, $username, $password);
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
+$connect = new DatabaseConnect();
 
 $sql = "CREATE DATABASE Dollar_Prices";
 if ($connection->query($sql) === TRUE) {
@@ -17,7 +10,7 @@ if ($connection->query($sql) === TRUE) {
     echo "Error creating database: " . $connection->error;
 }
 
-$sql = "CREATE TABLE Countries (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, code VARCHAR(30) NOT NULL,name VARCHAR(50) NOT NULL,value VARCHAR(30),
+$sql = "CREATE TABLE Countries (code VARCHAR(30) NOT NULL PRIMARY KEY,name VARCHAR(50) NOT NULL,value VARCHAR(30),
 curr_full VARCHAR(50) NOT NULL ,curr_short VARCHAR(30) NOT NULL,flag longblob NOT NULL)";
 if ($connection->query($sql) === TRUE) {
     echo "Countries created successfully";
